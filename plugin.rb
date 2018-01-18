84b27bfa591ec0db53949a5585b8cb5fad64b1c2
AlexRabolin <alexrabolin@yahoo.fr>
2018-01-18 04:59:50 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -3,12 +3,18 @@
 # authors: Daniel Fernández
 
 # freedom patch Google Calendar Onebox
-class Onebox::Engine::CalendarOnebox
-  include Onebox::Engine
+module Onebox
+  module Engine
+      include Engine
 
-  matches_regexp(/^https?:\/\/(?:www\.)?(?:m\.)?(?:google\.com\/calendar)\/.+$/)
+      matches_regexp(/^(https?:)?\/\/www\.meteoblue\.com\/.+/i);
+      matches_regexp(/^(https?:)?\/\/www\.windfinder\.com\/.+/i);
+      matches_regexp(/^(https?:)?\/\/www\.windguru\.cz\/.+/i);
+      matches_regexp(/^(https?:)?\/\/www\.google\.com\/maps\/.+/i);
+      always_https
 
-  def to_html
-    "<iframe src='#{@url}' style='border: 0' width='800' height='600' frameborder='0' scrolling='no'></iframe>"
-  end
-end
+      def to_html
+        url = @url.split('&').first
+        src = ::Onebox::Helpers.normalize_url_for_output(url)
+        "<iframe src='#{src}&rm=minimal' style='border: 0' width='800' height='600' frameborder='0' scrolling='no'></iframe>"
+      end
